931b3733b2d6d6f1efa6b514efc87908b6c468fd
Binigithub <42274807+Binigithub@users.noreply.github.com>
2019-01-08 19:07:51 -0600

    Update conf.js

--- CyberBank/Configuration/conf.js
+++ CyberBank/Configuration/conf.js
@@ -4,14 +4,33 @@ var HtmlReporter = require('protractor-beautiful-reporter');
 
 exports.config = {
 
-   directConnect : true,
+	// specs: ['../Tests/demo.spec.js'],
 
-//   capabilities: {
 
-//     browserName: 'chrome'
 
-//   },
-   multiCapabilities: [
+	suites:{
+
+
+
+		smoke:['../Tests/demo.spec.js'],
+
+		regression:['../Tests/*.spec.js']
+
+	},
+
+ 
+
+	sauceUser: process.env.SAUCE_USERNAME,
+
+	sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+
+
+	// restartBrowserBetweenTests: true,
+
+
+
+    multiCapabilities: [
 
 		{
 
@@ -45,70 +64,62 @@ exports.config = {
 
     }],
 
-  
-  //specs: ['../Tests/Test.js'],
-  //specs: ['../Tests/BankManagerSimple.spec.js'], 
-  //specs: ['../Tests/DataProvider.spec.js'],
-  //suites:{
-    //smoke: ['../Tests/BankManagerSimple.spec.js','../Tests/demo.spec.js'],
-   //regression: ['../Test/*.spec.js'],
-  //},
-suites:{
-   smoke:['../Tests/demo.spec.js'],
-   regression:['../Tests/*.spec.js']
+
+
+	onPrepare: function () {
+
+		// browser.ignoreSynchronization = true;	// if not an Angular app
+
+
+
+		jasmine.getEnv().addReporter({
+
+			specStarted: function(result) {
+
+				browser.getCapabilities().then(function (capabilities) {
+
+					var browserName = capabilities.get("browserName");
+
+					browser.executeScript("sauce:job-name=" + browserName + ":" + result.fullName);
+
+				});
+
+			}
+
+		});
+
+
+
+// 		var AllureReporter = require('jasmine-allure-reporter');
+
+// 		jasmine.getEnv().addReporter(new AllureReporter({
+
+// 		  resultsDir: 'allure-results'
+
+// 		}));
+
+
 
 	},
-   sauceUser: process.env.SAUCE_USERNAME,
-
-	sauceKey: process.env.SAUCE_ACCESS_KEY,
 
 
-onPrepare: function () {
 
-    browser.driver.manage().window().maximize();
+	onComplete: function () {
 
-    jasmine.getEnv().addReporter(new SpecReporter({
+		var printSessionId = function (jobName) {
 
-        displayFailuresSummary: true,
-        displayFailuredSpec: true,
-        displaySuiteNumber: true,
-        displaySpecDuration: true,
-        showstack: false
+			browser.getSession().then(function (session) {
 
-      }));
+				console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
 
-      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+			});
 
-      jasmine.getEnv().addReporter(new HtmlReporter({
+		}
 
-        baseDirectory: '../report/screenshots',
+		
 
-        preserveDirectory: false,
+		printSessionId("Insert Job Name Here");
 
-        screenshotsSubfolder: 'images',
-
-        jsonsSubfolder: 'jsons',
-
-        docName: 'CyberBank-Report.html'
-
-     }).getJasmine2Reporter());
-
-  
-
-},
-
-    
-
-    jasmineNodeOpts: {
-
-        showColors: true, 
-
-        defaultTimeoutInterval: 30000,    
-
-        print: function() {}
-
-        
-
-}
+	},
 
 };
